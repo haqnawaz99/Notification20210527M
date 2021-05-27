d405d949229852498b0203cad1a839738c1d1106
Haq Nawaz <haqnawaz99@yahoo.com>
2021-05-27 08:37:20 +0500

    Initial commit

---
+++ app/src/main/java/haqnawaz/org/notification20210527/MainActivity.java
@@ -0,0 +1,29 @@
+package haqnawaz.org.notification20210527;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.cert.CertPathBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
